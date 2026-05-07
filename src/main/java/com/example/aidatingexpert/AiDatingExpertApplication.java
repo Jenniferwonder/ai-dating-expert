910793de7d632448ef2eacba3012eebef2762f9a
Jenniferwonder <Jenniferwonder@163.com>
2026-05-07 13:19:00 +0800

    feat: init springboot project

---
+++ src/main/java/com/example/aidatingexpert/AiDatingExpertApplication.java
@@ -0,0 +1,13 @@
+package com.example.aidatingexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDatingExpertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDatingExpertApplication.class, args);
+    }
+
+}
