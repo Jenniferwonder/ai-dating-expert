c7e8efda08860b5c06db10bc256bb89981964b6d
Jenniferwonder <Jenniferwonder@163.com>
2026-05-25 20:54:17 +0800

    chore: initial test

--- src/main/java/com/jenni/aidatingexpert/AiDatingExpertApplication.java
+++ src/main/java/com/jenni/aidatingexpert/AiDatingExpertApplication.java
@@ -1,4 +1,4 @@
-package com.example.aidatingexpert;
+package com.jenni.aidatingexpert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/jenni/aidatingexpert/AiDatingExpertApplicationTests.java
+++ src/test/java/com/jenni/aidatingexpert/AiDatingExpertApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.aidatingexpert;
+package com.jenni.aidatingexpert;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
